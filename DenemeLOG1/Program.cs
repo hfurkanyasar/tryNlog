631468bcdccd64f284ccf11df987f382baa19133
hfurkanyasar <hfurkanyasar@gmail.com>
2023-07-14 18:00:17 +0300

    Add project files.

---
+++ DenemeLOG1/Program.cs
@@ -0,0 +1,49 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DenemeLOG1
+{
+    public class Program
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            //Per request
+            System.Diagnostics.Trace.CorrelationManager.ActivityId = Guid.NewGuid();
+
+            DoWork();
+
+        }
+
+        private static void DoWork()
+        {
+            //...
+
+            logger.Debug("DoWork method'u çalıştırıldı.");
+
+            DoOtherWork();
+            LimitUstuAvans();
+        }
+
+        private static void DoOtherWork()
+        {
+            //...
+
+            logger.Debug("DoOtherWork method'u çalıştırıldı.");
+        }
+
+
+        private static void LimitUstuAvans()
+        {
+
+            logger.Debug("Limit üstü avans çekimi yapıldı.");
+        
+        
+        }
+    }
+}
